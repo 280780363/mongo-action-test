2d05fd3020e6c4fb37b6d243c92ed1c52f4c39fa
gucao <280780363@qq.com>
2022-10-08 11:40:54 +0800

    init

---
+++ test/tests.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using Xunit;
+
+namespace test;
+
+public class Tests
+{
+    [Fact]
+    public void DoTest()
+    {
+        var mongoClient = new MongoClient("mongodb://mongo:123123@localhost:27017/?authSource=admin");
+        mongoClient.GetDatabase("test");
+    }
+}
